4ddb40d6d8674be35ba62c286b275012722e6491
lycuong99 <lycuong99@gmail.com>
2024-05-21 21:19:41 +0700

    update header

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,3 +1,13 @@
+import { BellRing } from "lucide-react";
+import { Button } from "../ui/button";
+
 export default function Header() {
-    return <div>Header</div>
+  return (
+    <header className="flex justify-between p-4">
+      <h1 className="text-2xl font-bold">Dashboard</h1>
+      <Button variant="ghost" className="p-2">
+        <BellRing className="h-5 w-5"/>
+      </Button>
+    </header>
+  );
 }
