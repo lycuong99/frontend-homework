65e8f0cfefcf1b01369323ef03e4f2041c424420
lycuong99 <lycuong99@gmail.com>
2024-05-21 21:11:19 +0700

    update nav

--- frontend/src/components/layout/Nav.tsx
+++ frontend/src/components/layout/Nav.tsx
@@ -1,6 +1,9 @@
 import useCheckActiveNav from "@/hooks/useCheckActiveNav";
 import { cn } from "@/lib/utils";
+import { LucideIcon } from "lucide-react";
 import Link from "next/link";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
+import { buttonVariants } from "../ui/button";
 
 const NavGroup = ({
   children,
@@ -22,47 +25,71 @@ const NavGroup = ({
 interface NavLink {
   title: string;
   href: string;
-  icon: any;
+  icon: LucideIcon;
   label?: string;
+  disabled?: boolean;
 }
-// const NavLink = ({title, href, icon, label}: NavLink) => {
-//     const {checkActiveNav} = useCheckActiveNav(href);
-//     return <Link href={href} className={
-//         cn(
-//             'group flex items-center rounded-md px-3 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground',
-//             path === link.href ? 'bg-accent' : 'transparent',
-//             item.disabled && 'cursor-not-allowed opacity-80'
-//           )
-//     }>
-//         <link.icon className="mr-2 h-4 w-4" />
-//         {title}
-//     </Link>
-// };
+const NavLink = ({ title, href, icon, label, disabled }: NavLink) => {
+  const { checkActiveNav } = useCheckActiveNav();
+  const Icon = icon;
+  return (
+    <Link
+      href={href}
+      className={cn(
+        "group flex justify-start items-center rounded-md px-3 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground",
+        checkActiveNav(href) ? "bg-accent" : "transparent",
+        disabled && "cursor-not-allowed opacity-80"
+      )}
+    >
+      <Icon className="mr-2 h-4 w-4" />
+      {title}
+    </Link>
+  );
+};
+
+const NavLinkCollapsed = ({ title, href, icon, label }: NavLink) => {
+  const { checkActiveNav } = useCheckActiveNav();
+  const Icon = icon;
+  return (
+    <Tooltip delayDuration={0}>
+      <TooltipTrigger asChild>
+        <Link
+          href="#"
+          className={cn(
+            buttonVariants({ variant: "ghost", size: "icon" }),
+            "h-9 w-9"
+            //   "default" === "default" &&
+            //     "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white"
+          )}
+        >
+          <Icon className="h-4 w-4" />
+          <span className="sr-only">{title}</span>
+        </Link>
+      </TooltipTrigger>
+      <TooltipContent side="right" className="flex items-center gap-4">
+        {title}
+        {label && (
+          <span className="ml-auto text-muted-foreground">{label}</span>
+        )}
+      </TooltipContent>
+    </Tooltip>
+  );
+};
 interface NavProps {
   isCollapsed: boolean;
   links: any[];
 }
 const Nav = ({ isCollapsed, links }: NavProps) => {
-  const { checkActiveNav } = useCheckActiveNav();
   return (
-    <>
+    <nav className="flex flex-col gap-1 px-2">
       {links.map((link) => {
-        return (
-          <Link
-            key={link.href}
-            href={link.href}
-            className={cn(
-              "group flex items-center rounded-md px-3 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground",
-            //   checkActiveNav(link.href) ? "bg-accent" : "transparent",
-              link.disabled && "cursor-not-allowed opacity-80"
-            )}
-          >
-            <link.icon className="mr-2 h-4 w-4" />
-            {link.title}
-          </Link>
+        return isCollapsed ? (
+          <NavLinkCollapsed key={link.href} {...link} />
+        ) : (
+          <NavLink key={link.href} {...link} />
         );
       })}
-    </>
+    </nav>
   );
 };
 
--- frontend/src/components/layout/index.tsx
+++ frontend/src/components/layout/index.tsx
@@ -1,23 +1,26 @@
-'use client';
+"use client";
 import { useState } from "react";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import { TooltipProvider } from "../ui/tooltip";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   const [isCollapsed, setIsCollapsed] = useState(false);
   return (
-    <div className="relative h-full overflow-hidden bg-background">
-      <Sidebar isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
-      <main
-        id="content"
-        className={`overflow-x-hidden pt-16 transition-[margin] md:overflow-y-hidden md:pt-0 ${
-          isCollapsed ? "md:ml-14" : "md:ml-64"
-        } h-full`}
-      >
-        <Header />
-        {children}
-      </main>
-    </div>
+    <TooltipProvider>
+      <div className="relative h-full overflow-hidden bg-background">
+        <Sidebar isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
+        <main
+          id="content"
+          className={`overflow-x-hidden pt-16 transition-[margin] md:overflow-y-hidden md:pt-0 ${
+            isCollapsed ? "md:ml-14" : "md:ml-64"
+          } h-full`}
+        >
+          <Header />
+          {children}
+        </main>
+      </div>
+    </TooltipProvider>
   );
 };
 
