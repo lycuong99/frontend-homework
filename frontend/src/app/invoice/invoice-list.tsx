478abec2ccc9727033abdcfad6093fdbc2219721
lycuong99 <lycuong99@gmail.com>
2024-05-22 23:56:38 +0700

    add tab filter

--- frontend/src/app/invoice/invoice-list.tsx
+++ frontend/src/app/invoice/invoice-list.tsx
@@ -2,10 +2,65 @@
 import { DataTable } from "@/components/data-table";
 import { columns } from "@/components/columns";
 import { useStore } from "@/store";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { useEffect, useMemo, useState } from "react";
+import { statuses } from "@/data/data";
+import { Badge } from "@/components/ui/badge";
+import { StatusBadge } from "@/components/status-badge";
+import { InvoiceStatus } from "@/types/invoice";
+
 
 const InvoiceList = () => {
   const { invoices } = useStore();
-  return <DataTable data={invoices} columns={columns} />;
+  const [tab, setTab] = useState("all");
+
+  const { invoicesFilter } = useMemo(() => {
+    if (tab === "all") {
+      return { invoicesFilter: invoices };
+    } else {
+      return {
+        invoicesFilter: invoices.filter((invoice) => invoice.status === tab),
+      };
+    }
+  }, [invoices, tab]);
+
+  function countStatus(status: string) {
+    if(status === "all") return invoices.length
+    return invoices.filter((invoice) => invoice.status === status).length;
+  }
+  return (
+    <>
+      <Tabs
+        orientation="vertical"
+        defaultValue="overview"
+        className="space-y-4"
+        value={tab}
+        onValueChange={setTab}
+      >
+        <div className="w-full overflow-x-scroll pb-2">
+          <TabsList>
+            <TabsTrigger value="all"  className="flex gap-2">All <Badge className="p-1 text-xs" variant={"default"}>{countStatus('all')}</Badge></TabsTrigger>
+
+            {statuses.map((status) => (
+              <TabsTrigger className="flex gap-2" key={status.value} value={status.value}>
+                {status.label} <StatusBadge className="p-1 py-0.5" status={status.value as InvoiceStatus}>{countStatus(status.value)}</StatusBadge>
+              </TabsTrigger>
+            ))}
+          </TabsList>
+         
+        </div>
+        <DataTable data={invoicesFilter} columns={columns} />
+      </Tabs>
+    </>
+  );
 };
 
 export default InvoiceList;
--- frontend/src/app/invoice/page.tsx
+++ frontend/src/app/invoice/page.tsx
@@ -2,7 +2,7 @@ import InvoiceList from "./invoice-list";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-16 py-10">
+    <main className="flex min-h-screen flex-col px-16 py-10">
       <InvoiceList />
     </main>
   );
